335b8bbf9cdcdadd0f32ec49c76e45ce8b986edd
tlscksgur <12880924a@gmail.com>
2026-06-29 18:56:13 +0900

    Express서버 오픈 성공

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
-const app = express;
+const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 app.use(express.static("public"));
 
-app.length("/", (req, res) => {
+app.get("/", (req, res) => {
   res.send("Business Card Backend Server");
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running at https://localhost:${PORT}`);
 })
