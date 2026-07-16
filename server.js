af6286f5076882f69e69f59e0f16d9a8e364b2f6
tlscksgur <12880924a@gmail.com>
2026-07-16 16:20:11 +0900

    호환문제해결

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
 const db = require("./database/db");
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
